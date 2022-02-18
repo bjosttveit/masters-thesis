2cbdb6b3dffd3c49c0402fd7957099fb560fe9f6
Bjørnar Østtveit <bjosttveit@gmail.com>
2022-02-18 14:17:12 +0100

    valkidate

--- copy_results.sh
+++ copy_results.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-scp -r bjornost@idun-login1.hpc.ntnu.no:/cluster/home/bjornost/masters-thesis/tests/ /Users/bjosttveit/Master/masters-thesis/tests/
+scp -r bjornost@idun-login1.hpc.ntnu.no:/cluster/home/bjornost/masters-thesis/tests /Users/bjosttveit/Master/masters-thesis/
